cee107d26e58669448a5d3a58c6ae7c0652fcc84
Taha Moumni <53491043+moumnitaha@users.noreply.github.com>
2023-09-25 14:43:22 +0100

    ex02

---
+++ ex02/Fixed.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmoumni <tmoumni@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/22 11:42:26 by tmoumni           #+#    #+#             */
+/*   Updated: 2023/09/25 14:42:16 by tmoumni          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+class Fixed
+{
+	private:
+		int _value;
+		static const int _fractional_bits;
+	public:
+		Fixed();
+		Fixed(const Fixed &Fxd);
+		Fixed(const int a);
+		Fixed(const float f);
+		Fixed &operator = (const Fixed &Fxd);
+		Fixed  operator + (const Fixed &Fxd);
+		Fixed  operator - (const Fixed &Fxd);
+		Fixed  operator * (const Fixed &Fxd);
+		Fixed  operator / (const Fixed &Fxd);
+
+		bool  operator > (const Fixed &Fxd);
+		bool  operator < (const Fixed &Fxd);
+		bool  operator >= (const Fixed &Fxd);
+		bool  operator <= (const Fixed &Fxd);
+		bool  operator == (const Fixed &Fxd);
+		bool  operator != (const Fixed &Fxd);
+
+		Fixed &operator ++ (void);
+		Fixed operator ++ (int);
+
+		static const Fixed &max( Fixed const &a,  Fixed const &b);
+
+		float toFloat( void ) const;
+		int toInt( void ) const;
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+		~Fixed();
+};
+
+std::ostream & operator<<(std::ostream & o, Fixed const & FixedPoint);
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmoumni <tmoumni@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/22 11:42:20 by tmoumni           #+#    #+#             */
+/*   Updated: 2023/09/25 13:51:56 by tmoumni          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void )
+{
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
+    return 0;
+}
